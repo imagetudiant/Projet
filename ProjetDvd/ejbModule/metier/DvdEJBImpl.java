92cb3ea9d9b4efc52a8bf529b7cd1ca22454da08
Moâde <Mode@172.23.5.248>
2016-11-14 23:01:34 +0100

    Modification des interfaces

--- ProjetDvd/ejbModule/metier/DvdEJBImpl.java
+++ ProjetDvd/ejbModule/metier/DvdEJBImpl.java
@@ -10,17 +10,27 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import metier.entities.Auteur;
 import metier.entities.Dvd;
+import metier.entities.Realisateur;
 
-@Stateless(name="DVD")
+@Stateless(name="dvd")
 public class DvdEJBImpl implements DvdLocal, DvdRemote {
 	@PersistenceContext(unitName="ProjetDvd")
 	private EntityManager em;
+	
+	@Override
+	public void editDvd(int id, String titre, double prix) {
+		Dvd d= getDvd(id);
+		d.setTitre(titre);
+		BigDecimal prixBD = BigDecimal.valueOf(prix);
+		d.setPrix(prixBD);
+	}
 	@Override
 	public Dvd getDvd(int id) {
 		Dvd d = em.find(Dvd.class, id);
 		if(d==null) throw new RuntimeException("Dvd introuvable");
-		return null;
+		return d;
 	}
 
 	@Override
@@ -34,6 +44,7 @@ public class DvdEJBImpl implements DvdLocal, DvdRemote {
 		}
 		return result;
 		}
+	
 	public List<Dvd> searchDvd(String titreRech) {
 		List<Dvd> result = new ArrayList<Dvd>();
 		if(titreRech == null) throw new RuntimeException("ERROR");
@@ -49,14 +60,48 @@ public class DvdEJBImpl implements DvdLocal, DvdRemote {
 		}
 		return result;
 	}
-	
-	
-	@Override
-	public void editDvd(int id, String titre, double prix) {
-		Dvd d= getDvd(id);
-		d.setTitre(titre);
-		BigDecimal prixBD = BigDecimal.valueOf(prix);
-		d.setPrix(prixBD);
+
+	public void setRealisateur(int id_realisateur){
+		Dvd d = em.find(Dvd.class, id_realisateur);
+		Realisateur r= d.getRealisateur();
+		em.persist(r);
 	}
+	
+	public void setAuteur(int id_auteur){
+		Dvd d = em.find(Dvd.class, id_auteur);
+		Auteur r= d.getAuteur();
+		em.persist(r);
+		}
+	
+	public void addDvd(Dvd d){
+		em.persist(d);
+	}
+    
+	
+	public void removeAll(){
+		List<Dvd> result = new ArrayList<Dvd>();
+		Iterator<Dvd> it = result.iterator();
+		while(it.hasNext()){
+			Dvd d = it.next();
+			em.remove(d);
+		}
+	}
+	
+	public void removeDvd(int id) {
+		List<Dvd> result = new ArrayList<Dvd>();
+		Iterator<Dvd> it = result.iterator();
+		 while(it.hasNext()){
+			 Dvd d = it.next();
+			 if (id == d.getId()){
+				 em.remove(d);
+			 }
+		 }
+	}
+	
+	//public void setdvdtitre(int id, String titre);
+	//public void setdvd (int id);
+	//public List<Dvd> getdvdbyauthor(long id_auteur) {}
+	//public List<Dvd> getdvdbyrealisateur(long id_realisateur) {}
+	
 
 }
--- ProjetDvd/ejbModule/metier/DvdLocal.java
+++ ProjetDvd/ejbModule/metier/DvdLocal.java
@@ -37,5 +37,16 @@ public interface DvdLocal {
 	 */
 	public void editDvd(int id, String titre, double prix);
 	
+	//public void creerDvd(String categorie, double prix, double stock, String titre, int auteur_id, int realisateur_id);
 
+
+	public void setRealisateur(int id_realisateur);
+	public void setAuteur(int id_auteur);
+	public void removeAll();
+	public void removeDvd(int id);
+	public void addDvd(Dvd d);
+	//public void setdvdtitre(int id, String titre);
+	//public void setdvd (int id);
+	//public List<Dvd> getdvdbyauthor(long id_auteur);
+	//public List<Dvd> getdvdbyrealisateur(long id_realisateur);
 }
