ac9cd551624833f5fc1494dcb076b3f8adbd9564
aahdih <hajar.aahdi@phelma.grenoble-inp.fr>
2016-11-22 18:36:20 +0100

    Revert "Ajout de la méthode addClient."
    This reverts commit 332a17607d89d9cc4cdc6cafa711d91bf05f8d77.

--- ProjetDvd/ejbModule/metier/ClientRemote.java
+++ ProjetDvd/ejbModule/metier/ClientRemote.java
@@ -40,7 +40,5 @@ public interface ClientRemote {
 	public void setSexe(String sexe);
 	
 	public boolean IsClient(String email);
-	
-	public void addClient(String email, String password, String nom, String prenom, String addresse, Date date, String sexe);
 
 }
